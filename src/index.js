ba54413a65189cae48ac034be2fa2e1a1c615906
vkamineni <Venkat.Kamineni@signetjewelers.com>
2021-03-18 05:35:51 -0400

    remove

--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.css";
 
-import * as serviceWorker from "./serviceWorker";
-
 import App from "./app";
 import "./index.css";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-serviceWorker.unregister();
