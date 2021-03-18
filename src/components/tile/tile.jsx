068a7badd94ffad7bca2a794cb635cdf7f78ed6f
vkamineni <Venkat.Kamineni@signetjewelers.com>
2021-03-18 04:48:07 -0400

    back ground color changes

--- src/components/tile/tile.jsx
+++ src/components/tile/tile.jsx
@@ -3,8 +3,10 @@ import React, { Component } from "react";
 import "./tile.css";
 class Tile extends Component {
   render() {
+    let colour = this.props.value === 'X' ? '#8F7A76' : this.props.value === 'O' ? '#D9D3D2' : '#FFFF';
     return (
       <button
+        style={{background : colour}}
         className={"tile " + this.props.value}
         onClick={this.props.onClick}
       >
